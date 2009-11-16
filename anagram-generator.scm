8a5e642c7447903eba28f486c30f24cbd318766c
Eduardo Bellani <ebellani@gmail.com>
2009-11-16 19:13:40 -0200

    first usable version, but without heavy testing yet

--- anagram-generator.scm
+++ anagram-generator.scm
@@ -96,6 +96,8 @@
               true)
 
 
+
+
 ;; subtract : hash hash -> hash or false
 ;; checks if a given word is contained
 ;; in the original, which means that an the given
@@ -116,19 +118,28 @@
           
           ;; subtract-acc : hash number -> hash
           (define (subtract-acc new-word index)
-            (let ([next-index (hash-iterate-next given-word index)]
-                  [key (hash-iterate-key given-word index)])
-              (cond
-                [(false? next-index) ;;last item
-                 (remove-key-or-value new-word key)]
-                [else
-                 (subtract-acc (remove-key-or-value new-word key)
-                               next-index)])))]
+            (cond
+              [(false? index) new-word]
+              [else
+               (let ([next-index (hash-iterate-next given-word index)]
+                     [key (hash-iterate-key given-word index)])
+                 (cond
+                   [(false? index) false]
+                   [(false? next-index) ;;last item
+                    (remove-key-or-value new-word key)]
+                   [else
+                    (subtract-acc (remove-key-or-value new-word key)
+                                  next-index)]))]))]
     (cond
       [(not (is-contained? given-word origin-word)) false]
       [else
        (subtract-acc origin-word
                      (hash-iterate-first given-word))])))
+
+
+(check-expect (subtract (build-lexical-analysis "hellol")
+                        (build-lexical-analysis ""))
+              (build-lexical-analysis "hellol"))
 
 (check-expect (subtract (build-lexical-analysis "hellol")
                         (build-lexical-analysis "hella"))
@@ -202,71 +213,113 @@
                              '("g" "ho" "st" "host"))
               '("st"))
 
+
 ;; PRECISO DE
 ;; UMA FUNCAO generate-all-anagrams 
 ;; GERAR UM NOVO LEXICOM PARA CADA PIRA
 
-;; generate-all-anagrams : hash (listof string) -> (listof (listof string))
+
+;; analyse-list : (listof string) -> hash
+;; build a lexical analysis of an entire lexicon
+(define (analyse-list los)
+  (build-lexical-analysis
+   (foldr string-append
+          ""
+          los)))
+
+;; hash-empty? : hash -> boolean
+;; checks if a hash is empty. For more readable code.
+(define (hash-empty? a-hash)
+  (false? (hash-iterate-first a-hash)))
+
+;; generate-all-anagrams : string (listof string) -> (listof (listof string)) or false
 ;; generates all the anagrams for a given word that are 
 ;; found in the lexicon. Returns false if no 
 ;; anagram is possible
 (define (generate-all-anagrams given-word dictionary)
-  (local [(define (generate-anagram word lexicon anagram-piece)
-            (cond
-              [(zero? (hash-count word)) anagram-piece]
-              [(empty? lexicon) empty] 
-              [else
-               (let* ([possible-anagram-increment
-                       (build-lexical-analysis 
-                        (foldr string-append
-                               ""
-                               (append anagram-piece
-                                       (list (first lexicon)))))]
-                      [word-rest (subtract given-word
-                                           possible-anagram-increment)])
-                 (cond
-                   [(false? word-rest) 
-                    (generate-anagram word
-                                      (rest lexicon)
-                                      anagram-piece)]
-                   [else
-                    (generate-anagram word-rest
-                                      (rest lexicon)
-                                      (append anagram-piece
-                                              (list (first lexicon))))]))]))
+  (local [;; check-for-anagram : (listof string) -> hash or false
+          ;; this serves to see if the last word on a possible anagram
+          ;; is itself a valid anagram. If it is use it for subtraction,
+          ;; else use the whole possible diagram list. It can return false if
+          ;; the subtraction fails
+;          (define (check-for-anagram possible-anagram)
+;            (let* ([initial-word
+;                    (build-lexical-analysis given-word)]
+;                   [result-for-last-word
+;                    (subtract initial-word
+;                              (build-lexical-analysis (last possible-anagram)))])
+;              (cond
+;                [(hash-empty? result-for-last-word)
+;                 result-for-last-word]
+;                [else
+;                 (subtract initial-word
+;                           (analyse-list possible-anagram))])))
           
-          (define (accumulate-anagrams lexicon anagram-accumulator)
-            (cond
-              [(empty? lexicon) anagram-accumulator]
-              [else
-               (let ([anagram (generate-anagram given-word
-                                                lexicon
-                                                empty)])
+          ;; find-anagram (listof string) (listof string) -> (listof string) or false
+          ;; discover if the the anagram accumulator matches the initial word, 
+          ;; because if so, it is an anagram. 
+          ;; Returns false if it is not and the lexicon is empty, because
+          ;; there is no more ways to search, but continue the search if it is not empty.
+          (define (find-anagram a-lexicon anagram-acc)
+            (let ([word-result (subtract (build-lexical-analysis given-word)
+                                         (analyse-list anagram-acc))])
+              (cond
+                [(false? word-result) false]
+                [(not (hash-empty? word-result))
                  (cond
-                   [(empty? anagram)
-                    (accumulate-anagrams (rest lexicon)
-                                         anagram-accumulator)]
-                   [else ;; found an anagram, but there could be others with this letter
-                    (accumulate-anagrams (rest lexicon)
-                                         (cons anagram
-                                               anagram-accumulator))]))]))]
+                   [(empty? a-lexicon) false]
+                   [else 
+                    (find-anagrams (build-lexicon word-result
+                                                  a-lexicon)
+                                   anagram-acc)])] ;; chamar outra func com o prox elemento
+                [else anagram-acc])));; we have found an anagram
+          
+          ;; find-anagrams : (listof string) (listof string) -> (listof (listof string)) or false
+          ;; Checks for the existence of anagrams for the anagram accumulator
+          ;; within the lexicon given.
+          ;; Uses the find-anagram for each entry of the new lexicon trying to find a 
+          ;; valid anagram. If one is not found return false an trackbacks
+          (define (find-anagrams a-lexicon anagram-acc)
+            (cond
+              [(empty? a-lexicon) false]
+              [(hash-empty? (subtract
+                             (build-lexical-analysis given-word)
+                             (build-lexical-analysis (first anagram-acc))))
+               (list (first anagram-acc))]
+              [else
+               (let ([possible-anagram
+                      (find-anagram a-lexicon
+                                    (append anagram-acc
+                                            (list (first a-lexicon))))])
+                 (cond
+                   [(false? possible-anagram)
+                    (find-anagrams (rest a-lexicon)
+                                   anagram-acc)]
+                   [else
+                    possible-anagram]))]))]
     
-    (accumulate-anagrams (build-lexicon given-word dictionary)
-                         empty)))
+    (filter-map (λ (dictionary-entry)
+                  (find-anagrams dictionary (list dictionary-entry)))
+                dictionary)))
 
 
 
-;(check-expect (generate-all-anagrams (build-lexical-analysis "ghost")
-;                                     '("g" "ho" "host" "st"))
-;              '('("g" "ho" "st")
-;                '("g" "host")))
+(generate-all-anagrams "ghost"
+                       '("tghos" "g" "ho" "host" "st"))
 
-;(check-expect (generate-all-anagrams (build-lexical-analysis "ghost")
-;                                     '("g" "ho" "st" "host"))
-;              '(("g" "ho" "st")
-;                ("g" "host")))
+;(check-expect (generate-all-anagrams "ghost"
+;                                     '("ghost" "g" "ho" "host" "st"))
+;              '(("ghost")
+;                ("g" "ho" "st")
+;                ("ho" "g" "st")
+;                ("host" "g")
+;                ("st" "g" "ho")))
 
-;(check-expect (generate-all-anagrams (build-lexical-analysis "web")
+;(check-expect (generate-all-anagrams "web"
+;                                     (list "web"))
+;              '(("web")))
+
+;(check-expect (generate-all-anagrams "web"
 ;                                     SMALL-DICTIONARY)
 ;              '("web"))
 ;
@@ -297,23 +350,23 @@
 
 ;; rotate-left : (listof X) number -> (listof X)
 ;; rotates N times to the left
-(define (rotate-left lox0 (times0 1))
-  (local [(define (rotate lox times)
-            (cond
-              [(or (<= times 0)
-                   (empty? (rest lox))) lox]
-              [(empty? lox) empty]
-              [else
-               (rotate (append (rest lox)
-                               (list (first lox)))
-                       (sub1 times))]))]
-    (rotate lox0 times0)))
-
-(check-expect (rotate-left '(1 2 3 4))
-              '(2 3 4 1))
-
-(check-expect (rotate-left '(1 2 3 4) 2)
-              '(3 4 1 2))
+;(define (rotate-left lox0 (times0 1))
+;  (local [(define (rotate lox times)
+;            (cond
+;              [(or (<= times 0)
+;                   (empty? (rest lox))) lox]
+;              [(empty? lox) empty]
+;              [else
+;               (rotate (append (rest lox)
+;                               (list (first lox)))
+;                       (sub1 times))]))]
+;    (rotate lox0 times0)))
+;
+;(check-expect (rotate-left '(1 2 3 4))
+;              '(2 3 4 1))
+;
+;(check-expect (rotate-left '(1 2 3 4) 2)
+;              '(3 4 1 2))
 
 
 (test)
